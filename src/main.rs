c86223334311a586b1a7f7a7deede53a0b5f1923
Daniel Drywa <daniel@drywa.me>
2017-02-26 01:39:18 +0000

    Added the basic rust version of reverse_word_pairs
    This commit doesn't include timings or attempts yet.
    However it already reads from the data file and spits out some output on the screen.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::iter::FromIterator;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+
+fn reverse_word( word : &str ) -> String {
+    String::from_iter( word.chars().rev() )
+}
+
+fn insert_word_pair( pairs : &mut HashMap<String, u32>, word : &str ) {
+    if let Entry::Occupied( mut x ) = pairs.entry( String::from( word ) ) {
+        *x.get_mut() += 1;
+        return;
+    }
+
+    pairs.entry( reverse_word( &word ) ).or_insert( 0 );
+}
+
+fn search_file( file : &File ) -> HashMap<String, u32> {
+    let mut pairs : HashMap<String, u32> = HashMap::new();
+    let mut word = String::with_capacity( 1024 );
+
+    for byte in file.bytes() {
+        let c = char::from( byte.unwrap() );
+
+        if c.is_whitespace() {
+            insert_word_pair( &mut pairs, &word );
+            word.clear();
+        } else {
+            word.push( c );
+        }
+    }
+
+    pairs
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open( "data/TheReverseWordTest.txt" ).unwrap();
+    let pairs = search_file( &file );
+
+    for ( key, count ) in pairs {
+        println!(
+            "{}/{} - {}",
+            key,
+            reverse_word( &key ),
+            count );
+    }
 }
