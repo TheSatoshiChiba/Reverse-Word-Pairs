02ea490821b5bf9b6d2bf1c1a3450fd0cc5b6c60
Daniel Drywa <daniel@drywa.me>
2017-02-27 10:26:23 +0000

    Added a repeatable run function

--- rust_attempts/src/lib.rs
+++ rust_attempts/src/lib.rs
@@ -3,12 +3,12 @@ pub mod attempt1;
 use std::fs::File;
 use std::time::Instant;
 
-pub fn run_attempt<F>( name : &str, attempt : F ) -> u64
+pub fn run<F>( name : &str, filename : &str, attempt : &F ) -> u64
     where F : Fn( &File ) -> usize {
 
     println!( "Starting attempt {}", name );
 
-    let file = File::open( "data/TheReverseWordTest.txt" ).unwrap();
+    let file = File::open( filename ).unwrap();
     let now = Instant::now();
     let result = attempt( &file );
     let duration = now.elapsed();
@@ -23,5 +23,31 @@ pub fn run_attempt<F>( name : &str, attempt : F ) -> u64
     time
 }
 
+pub fn repeat<F>( name : &str, filename : &str, count : u64, attempt : &F ) -> u64
+    where F : Fn( &File ) -> usize {
+
+    println!( "Starting repeated run of attempt {}. It will run {} time(s)", name, count );
+
+    let mut time : u64 = 0;
+
+    for _ in 0..count {
+        time += run( name, filename, attempt );
+    }
+
+    let average = time / count;
+
+    println!("Attempt {} stopped after {} runs with an average of {}ms", name, count, average );
+
+    average
+}
+
 pub fn run_all( count : u32, file : &File ) {
+    // let mut attempts = Vec::new();
+    // attempts.push( attempt1::run );
+
+    // let mut id = 0;
+    // for attempt in &attempts {
+    //     id += 1;
+    //     run( "Attempt #" + id, file, )
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 extern crate rust_attempts;
 
+use std::fs::File;
+
 fn main() {
-    let mut average : u64 = 0;
-    let count = 100;
-
-    for _ in 0..count {
-        average += rust_attempts::run_attempt( "#1", rust_attempts::attempt1::run );
-    }
-
-    println!("Average of {} runs: {}ms", count, average / count );
+    rust_attempts::repeat( "#1", "data/TheReverseWordTest.txt", 100, &rust_attempts::attempt1::run );
 }
