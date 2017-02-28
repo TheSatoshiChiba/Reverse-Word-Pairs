e7d883d32619a7a682263e62f7dc64dee3e26481
Daniel Drywa <daniel@drywa.me>
2017-02-28 17:49:44 +0000

    Added a bit of documentation

--- rust_attempts/src/lib.rs
+++ rust_attempts/src/lib.rs
@@ -1,19 +1,36 @@
+//! A module for reverse-word-pairs attempts.
+
 pub mod attempt1;
 
 use std::io::Write;
 use std::fs::File;
 use std::time::{Duration, Instant};
 
+/// The trait that returns the current struct as milliseconds.
 trait AsMilliseconds {
+    /// Returns the current struct as milliseconds.
     fn as_msecs( &self ) -> u64;
 }
 
+/// The `AsMilliseconds` trait implementation for `std::time::Duration`.
 impl AsMilliseconds for Duration {
+    /// Returns the current `std::time::Duration` as milliseconds.
     fn as_msecs( &self ) -> u64 {
         ( self.as_secs() * 1000 ) + ( self.subsec_nanos() / 1_000_000 ) as u64
     }
 }
 
+/// Flushes the standard console output.
+fn flush_console() {
+    std::io::stdout().flush().unwrap();
+}
+
+/// Runs a single attempt against a specified file name.
+///
+/// #Arguments
+///
+/// * `filename` - The name of the file to use for this attempt.
+/// * `attempt` - The attempt function of type `Fn( &File ) -> usize`.
 fn run_attempt<F>( filename : &str, attempt : &F ) -> ( usize, u64 )
     where F : Fn( &File ) -> usize {
 
@@ -24,6 +41,16 @@ fn run_attempt<F>( filename : &str, attempt : &F ) -> ( usize, u64 )
     ( result, now.elapsed().as_msecs() )
 }
 
+/// Runs a single attempt a given number of times and returns the average result.
+///
+/// This function also returns the amount of word pairs found. If between any
+/// run the number of found pairs differs it will return Err( &str ).
+///
+/// # Arguments
+///
+/// * `times` - The number of times the attempt will be run.
+/// * `filename` - The name of the file to use for this attempt.
+/// * `attempt` - The attempt function of type `Fn( &File ) -> usize`.
 fn repeat_attempt<F>( times : u64, filename : &str, attempt : &F ) -> Result<( usize, u64 ), &'static str>
     where F : Fn( &File ) -> usize {
 
@@ -43,12 +70,13 @@ fn repeat_attempt<F>( times : u64, filename : &str, attempt : &F ) -> Result<( u
             return Err( "Found pairs different from previous run." );
         }
 
-        std::io::stdout().flush().unwrap();
+        flush_console();
     }
 
     Ok( ( count, time / times ) )
 }
 
+/// Runs a given vector of attempts a given number of times and prints the results to the console.
 pub fn run<F>( attempts : &Vec<F>, times : u64, filename : &str )
     where F : Fn( &File ) -> usize {
 
