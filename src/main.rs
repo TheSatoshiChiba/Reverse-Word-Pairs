7e6fd31dbbce0c1385d0fd0dce8cd7eb36f42b8f
Daniel Drywa <daniel@drywa.me>
2017-03-05 23:41:20 +0000

    Fixed mutable vec warning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate rust_attempts;
 
 fn main() {
-    let mut attempts = vec![ rust_attempts::attempt1::run ];
+    let attempts = vec![ rust_attempts::attempt1::run ];
 
     rust_attempts::run( &attempts, 100, "data/TheReverseWordTest.txt" );
 }
